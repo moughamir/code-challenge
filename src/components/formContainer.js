e47cc3368b6ebaeb48e75cc55fd1ab30266f9d5a
Mohamed Moughamir <moughamir@gmail.com>
2019-03-19 13:54:41 +0000

    Add FormContainer

---
+++ src/components/formContainer.js
@@ -0,0 +1,10 @@
+import React from  'react'
+
+const FormContainer = (props) => {
+    return (
+    <div classname='form-group'>
+    {props.children}
+    </div>)
+        
+}
+export default FormContainer
