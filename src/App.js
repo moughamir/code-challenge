824484ecbaf876dae9b12d5827f44a3144658675
Mohamed Moughamir <moughamir@gmail.com>
2019-03-19 14:03:23 +0000

    Implement Form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { SignupAction } from './actions/auth'
+import FormContainer from './components/formContainer'
+import InputField from './components/inputField'
+import Button from './components/button'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <FormContainer>
+          <InputField type='text' placeholder='username'/>
+          <InputField type='email' placeholder='your email'/>
+          <InputField type='password'/>
+          <Button type='submit' text='sign up' handleClick={(e) => console.log(e)}/>
+        </FormContainer>
       </div>
     );
   }
